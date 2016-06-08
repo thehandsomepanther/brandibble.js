d85274525f99807cb230bdfcb32c1db58482fd2d
Hugh Francis <me@hughfrancis.me>
2016-06-08 14:33:21 -0400

    Yup

--- spec/brandibble.spec.js
+++ spec/brandibble.spec.js
@@ -1,8 +1,17 @@
 var expect = require('chai').expect;
 var Brandibble = require('..').default;
 
+// Test Helpers
+function seedEmail() {
+  return `sanctuary-testing-${Math.floor(Math.random()*90+10)}@example.com`;
+}
+
 // Init Brandibble Session for Testing
-var BrandibbleInstance = new Brandibble();
+var BrandibbleInstance = new Brandibble({
+  apiKey: '***REMOVED***',
+  brandId: 6,
+  apiEndpoint: 'http://unsecure.brandibble.co/api/'
+});
 
 // Brandibble Wrapper
 describe('Brandibble', () => {
@@ -22,6 +31,22 @@ describe('Brandibble.Customers', () => {
   it('exists', () => { expect(BrandibbleInstance.Customers).to.exist });
 
   it('stores the customer token', () => { expect(BrandibbleInstance.Customers.customerToken).to.exist });
+
+  it('can create a customer', () => {
+    BrandibbleInstance.Customers.create({
+      first_name: 'Sanctuary',
+      last_name: 'Testing',
+      email: seedEmail(),
+      password: 'password'
+    }).then(response => {
+      expect(response).to.be.a('object');
+      expect(response).to.have.property('customer_id');
+      expect(response).to.have.property('email');
+      expect(response).to.have.property('first_name');
+      expect(response).to.have.property('last_name');
+      expect(response).to.have.property('phone');
+    });
+  });
 });
 
 // Locations
