d8944f2546d77858adfb485d55add2b70de3e893
Hugh Francis <me@hughfrancis.me>
2016-06-27 13:32:39 -0400

    Update tests for new brandibble changes

--- spec/helpers.js
+++ spec/helpers.js
@@ -4,13 +4,15 @@ export function seedEmail() {
   return `sanctuary-testing-${(new Date()).valueOf().toString()}@example.com`;
 }
 
+export const UnsecureApiKey = 'eyJhbGciOiJIUzI1NiIsImV4cCI6MTQ5ODEzNTc2NiwiaWF0IjoxNDY2NTk5NzY2fQ.eyJlbWFpbCI6Imh1Z2hAc2FuY3R1YXJ5LmNvbSIsImRvbWFpbiI6Imh0dHBzOi8vc2FuY3R1YXJ5LmNvbXB1dGVyIiwiYXBpX3VzZXJfaWQiOjEsIm5hbWUiOiJTYW5jdHVhcnkifQ.ZiQdEYSawYF7v-ZO2dkOzY4WnMFIf67GScYfsxab-vw';
+
 export function seedText() {
   return `Testing ${(new Date()).valueOf().toString()}`;
 }
 
 export function buildRef() {
   return new Brandibble({
-    apiKey: '***REMOVED***',
+    apiKey: UnsecureApiKey,
     brandId: 6,
     apiEndpoint: 'http://unsecure.brandibble.co/api/'
   });
