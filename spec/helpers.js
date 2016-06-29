f416e62b01a4e3f4464f71669350f2f338f63106
Hugh Francis <hughfrancis89@gmail.com>
2016-06-29 15:49:08 -0400

    Feature/addresses (#4)
    * Update tests for new brandibble changes
    
    * Add Customer Address Endpoints
    
    * Update Readme
    
    * Typo

--- spec/helpers.js
+++ spec/helpers.js
@@ -1,8 +1,17 @@
 import Brandibble from '..';
 
+export const TestingUser = {
+  first_name: 'Sanctuary',
+  last_name: 'Testing',
+  email: 'sanctuary-testing-customer@example.com',
+  password: 'password'
+};
+
 export function seedEmail() {
   return `sanctuary-testing-${(new Date()).valueOf().toString()}@example.com`;
 }
+
+export const UnsecureApiKey = 'eyJhbGciOiJIUzI1NiIsImV4cCI6MTQ5ODU4MzQ0MiwiaWF0IjoxNDY3MDQ3NDQyfQ.eyJlbWFpbCI6Imh1Z2hAc2FuY3R1YXJ5Y29tcHV0ZXIuY29tIiwiZG9tYWluIjoiaHR0cHM6Ly9zYW5jdHVhcnkuY29tcHV0ZXIiLCJhcGlfdXNlcl9pZCI6MywibmFtZSI6IlNhbmN0dWFyeSJ9.clbyJWm-gj4Z120isZhL-Zk1Voy80pJJWsHfgnqGaxk';
 
 export function seedText() {
   return `Testing ${(new Date()).valueOf().toString()}`;
@@ -10,7 +19,7 @@ export function seedText() {
 
 export function buildRef() {
   return new Brandibble({
-    apiKey: 'c2FuY3R1YXJ5IGNvbXB1dGVy',
+    apiKey: UnsecureApiKey,
     brandId: 6,
     apiEndpoint: 'http://unsecure.brandibble.co/api/'
   });
