4a7da48e20f4afc50dc9cea24801687b73504e6e
Hugh Francis <hugh@sanctuary.computer>
2016-08-30 15:20:31 -0400

    Feature/113 order rehydration (#11)
    * Move over to LocalForage
    
    * Basic Rehydration
    
    * Update README

--- spec/setup.spec.js
+++ spec/setup.spec.js
@@ -1,19 +1,30 @@
-import { buildRef, shouldSucceed, TestingUser, TestingAddress } from './helpers';
-let BrandibbleRef = buildRef();
+import Brandibble from '..';
+import { shouldSucceed, TestingUser, TestingAddress, UnsecureApiKey } from './helpers';
 const { email, password } = TestingUser;
 
+
 function ensureCustomerResourcesExist() {
-  return BrandibbleRef.customers.authenticate({ email, password }).then(() => {
-    return BrandibbleRef.addresses.all().then(response => {
+  return window.Brandibble.customers.authenticate({ email, password }).then(() => {
+    return window.Brandibble.addresses.all().then(response => {
       let addresses = shouldSucceed(response);
-      if (addresses.length === 0) { return BrandibbleRef.addresses.create(TestingAddress); };
+      if (addresses.length === 0) { return window.Brandibble.addresses.create(TestingAddress); };
       return addresses[0];
     });
-  });
+  }).catch(error => console.log(error));
 }
 
 before(done => {
-  BrandibbleRef.customers.create(TestingUser)
-    .then(ensureCustomerResourcesExist, ensureCustomerResourcesExist)
-    .then(() => done());
+  // Setup a Brandibble Ref, and add it to the Window
+  let BrandibbleRef = new Brandibble({
+    apiKey: UnsecureApiKey,
+    brandId: 6,
+    apiEndpoint: 'https://staging.brandibble.co/api/'
+  });
+
+  return BrandibbleRef.setup().then(BrandibbleRef => {
+    window.Brandibble = BrandibbleRef;
+    BrandibbleRef.customers.create(TestingUser)
+      .then(ensureCustomerResourcesExist, ensureCustomerResourcesExist)
+      .then(() => done());
+  });
 });
