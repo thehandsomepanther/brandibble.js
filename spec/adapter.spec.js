9b151fccf5702b4dccfe0f2e1d8358600e6ecb48
Hugh Francis <me@hughfrancis.me>
2016-08-30 14:37:22 -0400

    Move over to LocalForage

--- spec/adapter.spec.js
+++ spec/adapter.spec.js
@@ -1,10 +1,33 @@
-import { seedEmail, buildRef, UnsecureApiKey } from './helpers';
-let BrandibbleRef = buildRef();
+import { seedEmail, UnsecureApiKey, configureTestingOrder } from './helpers';
 
 describe('Adapter', () => {
+  beforeEach(done => {
+    Brandibble.adapter.flushAll().then(done);
+  });
+
   it('builds the correct headers', () => {
-    let headers = BrandibbleRef.adapter.headers();
+    let headers = Brandibble.adapter.headers();
     expect(headers).to.have.property('Content-Type', 'application/json');
     expect(headers).to.have.property('Brandibble-Api-Key', UnsecureApiKey);
   });
+
+  it('can persist currentOrder to localStorage', done => {
+    configureTestingOrder(Brandibble).then(order => {
+      Brandibble.adapter.persistCurrentOrder(order).then(savedOrder => {
+        expect(order).to.equal(savedOrder);
+        done();
+      });
+    });
+  })
+
+  it('can restore currentOrder from localStorage', () => {
+    configureTestingOrder(Brandibble).then(order => {
+      Brandibble.adapter.persistCurrentOrder(order).then(() => {
+        Brandibble.adapter.restoreCurrentOrder().then(retrievedOrder => {
+          expect(order).to.equal(retrievedOrder);
+          done();
+        });
+      });
+    });
+  })
 });
