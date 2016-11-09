645e9b4e47ad99620da6cfc6ccf17141d9fff053
keithpops <keith.poplawski@gmail.com>
2016-11-08 20:38:15 -0500

    oops

--- spec/allergens.spec.js
+++ spec/allergens.spec.js
@@ -4,7 +4,6 @@ describe('Allergens', () => {
   it('exists', () => { expect(Brandibble.allergens).to.exist });
 
   it('can show all allergens', done => {
-    console.log(Brandibble)
     Brandibble.allergens.all().then(response => {
       let data = shouldSucceed(response);
       expect(data).to.be.a('array');
