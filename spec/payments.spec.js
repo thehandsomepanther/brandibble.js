683a273d17e1f2daf3810850a9335f821974093a
Sebastian <sebastian@sanctuary.computer>
2016-12-07 12:50:29 -0500

    added setDefault to payments (#32)

--- spec/payments.spec.js
+++ spec/payments.spec.js
@@ -54,11 +54,11 @@ describe('Payments', () => {
       password
     }).then(response => {
       let data = shouldSucceed(response);
-      Brandibble.payments.create({ 
-        'cc_number': 4788250000121443, 
-        'cc_expiration': 1018, 
-        'cc_cvv': 740, 
-        'cc_zip': 10022 
+      Brandibble.payments.create({
+        'cc_number': 4788250000121443,
+        'cc_expiration': 1018,
+        'cc_cvv': 740,
+        'cc_zip': 10022
       }).then(response => {
         let data = shouldSucceed(response);
         expect(data).to.be.a('array');
@@ -74,15 +74,50 @@ describe('Payments', () => {
       password
     }).then(response => {
       let data = shouldSucceed(response);
-      Brandibble.payments.create({ 
-        'cc_number': 478, 
-        'cc_expiration': 1018, 
+      Brandibble.payments.create({
+        'cc_number': 478,
+        'cc_expiration': 1018,
         'cc_cvv': 740,
-        'cc_zip': 10022 
+        'cc_zip': 10022
       }).catch(response => {
         let errors = shouldError(response);
         expect(errors).to.be.a('array').to.have.lengthOf(2);
         expect(errors[0]).to.have.property('code', 'customers.cards.add.invalid_card');
+        done();
+      });
+    });
+  });
+
+  it('can set a new default card', done => {
+    const { email, password } = TestingUser;
+    Brandibble.customers.authenticate({
+      email,
+      password
+    }).then(response => {
+      let data = shouldSucceed(response);
+      Brandibble.payments.all().then(response => {
+        let notDefault = response.data.filter(card => card.is_default === false)[0];
+        let { customer_card_id } = notDefault;
+        Brandibble.payments.setDefault(customer_card_id).then(response => {
+          expect(response).to.be.true;
+          done();
+        });
+      });
+    });
+  });
+
+  it('will fail when an invalid card id is passed to setDefault', done => {
+    const { email, password } = TestingUser;
+    Brandibble.customers.authenticate({
+      email,
+      password
+    }).then(response => {
+      let data = shouldSucceed(response);
+      let badId = 'hfg7d8fh';
+      Brandibble.payments.setDefault(badId).catch(response => {
+        let errors = shouldError(response);
+        expect(errors).to.be.a('array').to.have.lengthOf(2);
+        expect(errors[0]).to.have.property('code', 'customers.cards.update.invalid_id');
         done();
       });
     });
