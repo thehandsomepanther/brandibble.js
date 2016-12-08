0b236d5958fbca10a1e1b6f08c89ef145cb0dca6
sepowitz <sebastianodell@gmail.com>
2016-12-08 17:17:06 -0500

    tests a passing

--- spec/orders.spec.js
+++ spec/orders.spec.js
@@ -22,8 +22,8 @@ describe('Orders', () => {
           cc_zip: 12345,
           cc_cvv: 123
         };
-        return configureTestingOrder(Brandibble, customer, address)
-          .then(testingOrder => Brandibble.orders.submit(testingOrder, "credit", card))
+        return configureTestingOrder(Brandibble, customer, address, card)
+          .then(testingOrder => Brandibble.orders.submit(testingOrder))
           .then(response => {
             let data = shouldSucceed(response);
             expect(data).to.be.a('object');
@@ -32,4 +32,34 @@ describe('Orders', () => {
       });
     });
   });
+
+
+  it('will not restore a credit card submitted in it\'s raw format from local storage', done => {
+    const { email, password } = TestingUser;
+    Brandibble.customers.authenticate({
+      email,
+      password
+    }).then(response => {
+      let customer = shouldSucceed(response);
+      Brandibble.addresses.all().then(response => {
+        let address = shouldSucceed(response)[0];
+        let card = {
+          cc_expiration: '0130',
+          cc_number: Brandibble.TestCreditCards.visa[0].number,
+          cc_zip: 12345,
+          cc_cvv: 123
+        };
+        return configureTestingOrder(Brandibble, customer, address, card).then(order => {
+          Brandibble.adapter.persistCurrentOrder(order).then(() => {
+            Brandibble.adapter.restoreCurrentOrder().then(retrievedOrder => {
+              expect(retrievedOrder.creditCard).to.equal(null);
+              expect(retrievedOrder).to.be.an.instanceof(Brandibble.Order);
+              done();
+            });
+          });
+        }).catch(error => console.log(error));
+      });
+    });
+  })
+
 });
