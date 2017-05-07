8155dc0315fd1db3c534abfadfd4f0deaa5eb969
Sebastian Odell <sebastianodell@Sebastians-MacBook-Pro.local>
2017-05-07 14:51:03 -0400

    uses real prouct data

--- spec/stubs/menu.stub.js
+++ spec/stubs/menu.stub.js
@@ -1,35 +1,11 @@
+import productStub from './product.stub.js';
+
 export default [
   {
     children: [
       {
         id: 123,
-        items: [
-          {
-            name: 'Charred Chicken',
-            id: 12317,
-            option_groups: [
-              {
-                id: 473,
-                option_items: [
-                  {
-                    id: 9432,
-                  },
-                ],
-              },
-              {
-                id: 474,
-                option_items: [
-                  {
-                    id: 9436,
-                  },
-                  {
-                    id: 17136,
-                  },
-                ],
-              },
-            ],
-          },
-        ],
+        items: [productStub],
       },
     ],
   },
